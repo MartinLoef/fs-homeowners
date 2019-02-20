4b9ad46eb639dabf2c26d74817f291a282bdd2b3
Martin Loef <loefmartin@gmail.com>
2019-02-20 08:42:32 +0000

    added blog models, urls, basic views, admin config  and templates

---
+++ blogs/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url
+from .views import get_blogs, blog_detail, create_or_edit_blog, blog_like
+
+urlpatterns = [
+    url(r'^posts/$', get_blogs, name="get_blogs"),
+    url(r'^(?P<pk>\d+)/$', blog_detail, name='blog_detail'),
+    url(r'^(?P<pk>\d+)/like/$', blog_like, name='blog_like'),
+    url(r'^new/$', create_or_edit_blog, name='new_blog'),
+    url(r'^(?P<pk>\d+)/edit/$', create_or_edit_blog, name='edit_blog')
+    ]
