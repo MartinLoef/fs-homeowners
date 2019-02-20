f42a28b95b258accc96de889b9d24516d8c6d32d
Martin Loef <loefmartin@gmail.com>
2019-02-20 08:53:43 +0000

    added blogpostform and blogcomment form

--- blogs/forms.py
+++ blogs/forms.py
@@ -4,7 +4,7 @@ from .models import Blog, BlogComment
 class BlogPostForm(forms.ModelForm):
     class Meta:
         model = Blog
-        fields = ("title", "content", "published_date")
+        fields = ("title", "content")
         
 class BlogCommentForm(forms.Form):
     Blog_comment = forms.CharField(100)
