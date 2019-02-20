e634980f06af1bd69a5d2f48367a22c918ab20fd
Martin Loef <loefmartin@gmail.com>
2019-02-20 08:42:32 +0000

    added blog models, urls, basic views, admin config  and templates

--- blogs/models.py
+++ blogs/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Blog(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_author")
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    published_date = models.DateTimeField(blank=True, null=True, default=timezone.now)
+    views = models.IntegerField(default=0)
+    
+    def __str__(self):
+        return "BlogID: {0} / User: {1} / Date: {2} / Title: {3}".format(
+            self.id, self.authorid, self.created_date, self.title)
+
+class BlogComment(models.Model):
+    blogid = models.ForeignKey(Blog, on_delete=models.CASCADE, related_name="comment_blog")
+    authorid = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comment_author")
+    blog_comment = models.CharField(max_length=125)
+    created_date = models.DateTimeField(auto_now_add=True)
+    views = models.IntegerField(default=0)
+    likes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return "BlogCommentID: {0} / User: {1} / Date: {2}".format(
+            self.id, self.authorid, self.created_date)
+
+class BlogLike(models.Model):
+    BlogLikeId = models.ForeignKey(Blog, on_delete=models.CASCADE, related_name="blog_like")
+    BlogLikedBy = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_liked_by_user")
+    
+    def __int__(self):
+        return "BlogLikeId: {0} / BlogID: {1} / User: {2}".format(
+            self.id, self.BlogLikeId, self.user)
--- blogs/views.py
+++ blogs/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
+from .models import Blog, BlogComment, BlogLike, User
+from accounts.models import UserProfile
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+@login_required
+
+def get_blogs(request):
+    """blogposts.html"""
+    
+    return render(request, "blogposts.html")
+    
+def blog_like(request, pk):
+    """blogdetail.html"""
+    return redirect(blog_detail)
+    
+def blog_detail(request, pk):
+    """blogdetail.html"""
+    return render(request, "blogposts.html")
+    
+def create_or_edit_blog(request, pk=None):
+    """blogpostform.html"""
+    return render(request, "blogpostform.html")
+    
