795d861a5a785325b919dcf264a14051d6b84063
Martin Loef <loefmartin@gmail.com>
2019-03-10 14:59:38 +0000

    added edit and delete dropdown in eventdetails.html and added  delete_event view

--- events/urls.py
+++ events/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url
-from .views import create_or_edit_event, get_events, event_detail, event_like, event_comment, event_join
+from .views import create_or_edit_event, get_events, event_detail, event_like, event_comment, event_join, delete_event
 
 urlpatterns = [
     url(r'^events/$', get_events, name="get_events"),
@@ -9,4 +9,5 @@ urlpatterns = [
     url(r'^(?P<pk>\d+)/join/$', event_join, name='event_join'),
     url(r'^(?P<pk>\d+)/comment/$', event_comment, name='event_comment'),
     url(r'^(?P<pk>\d+)/edit/$', create_or_edit_event, name='edit_event'),
+    url(r'^(?P<pk>\d+)/delete/$', delete_event, name='delete_event'),
     ]
