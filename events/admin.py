c4215098bf2e1f40f3ac89c36ea66c5e80ceb6c7
Martin Loef <loefmartin@gmail.com>
2019-03-10 13:39:34 +0000

    added join functionality when events are free of charge

--- events/admin.py
+++ events/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Event, EventComment, EventLike
+from .models import Event, EventComment, EventLike, EventJoin
 
 admin.site.register(Event)
 admin.site.register(EventComment)
 admin.site.register(EventLike)
+admin.site.register(EventJoin)
--- events/urls.py
+++ events/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import url
-from .views import create_or_edit_event, get_events, event_detail, event_like, event_comment
+from .views import create_or_edit_event, get_events, event_detail, event_like, event_comment, event_join
 
 urlpatterns = [
     url(r'^events/$', get_events, name="get_events"),
     url(r'^(?P<pk>\d+)/$', event_detail, name='event_detail'),
     url(r'^new/$', create_or_edit_event, name='new_event'),
     url(r'^(?P<pk>\d+)/like/$', event_like, name='event_like'),
+    url(r'^(?P<pk>\d+)/join/$', event_join, name='event_join'),
     url(r'^(?P<pk>\d+)/comment/$', event_comment, name='event_comment'),
     url(r'^(?P<pk>\d+)/edit/$', create_or_edit_event, name='edit_event'),
     ]
