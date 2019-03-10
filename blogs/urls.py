e3a30b3186d884babc382105ddffd689b56458cb
Martin Loef <loefmartin@gmail.com>
2019-03-10 10:42:07 +0000

    cleaned up code blog views and added blogpost_comment view

--- blogs/urls.py
+++ blogs/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url
-from .views import get_blogs, blog_detail, create_or_edit_blog, blog_like
+from .views import get_blogs, blog_detail, create_or_edit_blog, blog_like, blogpost_comment
 
 urlpatterns = [
     url(r'^blogs/$', get_blogs, name="get_blogs"),
     url(r'^(?P<pk>\d+)/$', blog_detail, name='blog_detail'),
     url(r'^(?P<pk>\d+)/like/$', blog_like, name='blog_like'),
+    url(r'^(?P<pk>\d+)/comment/$', blogpost_comment, name='blogpost_comment'),
     url(r'^new/$', create_or_edit_blog, name='new_blog'),
     url(r'^(?P<pk>\d+)/edit/$', create_or_edit_blog, name='edit_blog')
     ]
