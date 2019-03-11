36d52fb22ea071ffce35ccadaf5e3f50e8862a08
Martin Loef <loefmartin@gmail.com>
2019-03-11 13:08:06 +0000

    added postgres database for heroku

--- env.py
+++ env.py
@@ -1,1 +1,8 @@
+import os
 
+os.environ.setdefault('EMAIL_ADDRESS', "commonholdproject@gmail.com")
+os.environ.setdefault('EMAIL_PASSWORD', "Commonhold")
+os.environ.setdefault('DATABASE_URL', "postgres://gnbuvzcvjvxzqp:f28151d2cbd4f02c29c181e500de62f249f6f099f7a7432755cfe764e4cd2e83@ec2-54-228-252-67.eu-west-1.compute.amazonaws.com:5432/ddmjpi23gnhrir")
+os.environ.setdefault('SECRET_KEY', "'17ecx98^hn+7b(ma&kddm_&jwjc)&3px(1e!o4=5zuql$=x@7k'")
+os.environ.setdefault('STRIPE_PUBLISHABLE', "'pk_test_Fzmde77QxCUaso4fUXas3Aia'")
+os.environ.setdefault('STRIPE_SECRET', "'sk_test_DAHIDBBpORcEO0oJdFIiMA4j'")
