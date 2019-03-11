19e7d1fc49ad07bf9ee5284017dd3376f40b2bc1
Martin Loef <loefmartin@gmail.com>
2019-03-11 11:13:34 +0000

    delete comment post functionality added

--- blogs/urls.py
+++ blogs/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url
-from .views import get_blogs, blog_detail, create_or_edit_blog, blog_like, blogpost_comment, delete_blog
+from .views import get_blogs, blog_detail, create_or_edit_blog, blog_like, blogpost_comment, delete_blog, delete_comment
 
 urlpatterns = [
     url(r'^blogs/$', get_blogs, name="get_blogs"),
@@ -9,4 +9,5 @@ urlpatterns = [
     url(r'^new/$', create_or_edit_blog, name='new_blog'),
     url(r'^(?P<pk>\d+)/edit/$', create_or_edit_blog, name='edit_blog'),
     url(r'^(?P<pk>\d+)/delete/$', delete_blog, name='delete_blog'),
+    url(r'^(?P<pk>\d+)/comment/delete/$', delete_comment, name='delete_comment'),
     ]
