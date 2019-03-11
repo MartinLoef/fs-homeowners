2c23360b5c44d55585bb24fdb710411254d99fd4
MartinLoef <43035810+MartinLoef@users.noreply.github.com>
2019-03-11 13:20:35 +0100

    Delete env.py

--- env.py
+++
@@ -1,5 +0,0 @@
-import os
-
-os.environ.setdefault("SECRET_KEY", "'17ecx98^hn+7b(ma&kddm_&jwjc)&3px(1e!o4=5zuql$=x@7k'")
-os.environ.setdefault("EMAIL_ADDRESS", "commonholdproject@gmail.com")
-os.environ.setdefault("EMAIL_PASSWORD", "Commonhold")
