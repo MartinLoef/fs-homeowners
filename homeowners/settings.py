8b999ed00ac2b678439f34a8031b2a6ec3e7f3d6
Martin Loef <loefmartin@gmail.com>
2019-03-12 20:08:22 +0000

    removed a old  media url in settings file

--- homeowners/settings.py
+++ homeowners/settings.py
@@ -167,9 +167,6 @@ STATICFILES_DIRS = [
 STRIPE_PUBLISHABLE = os.getenv('STRIPE_PUBLISHABLE')
 STRIPE_SECRET = os.getenv('STRIPE_SECRET')
 
-
-
-MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 EMAIL_USE_TLS = True
