3ac0e3bf68d01b1fbd16ea84df0c59b22645be60
Martin Loef <loefmartin@gmail.com>
2019-03-14 14:38:36 +0000

    manual test fixes for cart url and reverse redirect if not authenticated

--- cart/views.py
+++ cart/views.py
@@ -6,61 +6,72 @@ def view_cart(request):
     """
     Renders the cart contents page
     """
-    return render(request, "cart.html")
+    if request.user.is_authenticated:
+        return render(request, "cart.html")
+    else:
+        return redirect(reverse('index'))
     
     
 def add_to_cart(request, id):
     """
     Add a quantity of the specified event to the cart
     """
-    quantity=int(1)
-    cart = request.session.get('cart', {})
-    
-    if id in cart:
-        cart[id] += quantity
+    if request.user.is_authenticated:
+        quantity=int(1)
+        cart = request.session.get('cart', {})
+        
+        if id in cart:
+            cart[id] += quantity
+        else:
+            cart[id] = cart.get(id, quantity)
+        
+        request.session['cart'] = cart
+        return redirect(reverse('view_cart'))
     else:
-        cart[id] = cart.get(id, quantity)
-    
-    request.session['cart'] = cart
-    return redirect(reverse('view_cart'))
-    
+        return redirect(reverse('index'))
     
 def adjust_cart(request, id):
     """
     Adjust the quantity of the specified event to the specified amount
     """
-    if request.POST.get('quantity') == '':
-        request.session['cart'] = request.session.get('cart', {})
+    if request.user.is_authenticated:
+        if request.POST.get('quantity') == '':
+            request.session['cart'] = request.session.get('cart', {})
+            return redirect(reverse('view_cart'))
+        else:
+            quantity = int(request.POST.get('quantity'))
+            cart = request.session.get('cart', {})
+        
+        if quantity > 0:
+            cart[id] = quantity
+            print(cart)
+        else:
+            cart.pop(id)
+            
+        request.session['cart'] = cart
         return redirect(reverse('view_cart'))
     else:
-        quantity = int(request.POST.get('quantity'))
-        cart = request.session.get('cart', {})
-    
-    if quantity > 0:
-        cart[id] = quantity
-        print(cart)
-    else:
-        cart.pop(id)
-        
-    request.session['cart'] = cart
-    return redirect(reverse('view_cart'))
+        return redirect(reverse('index'))
     
 
 def adjust_cart_checkout(request, id):
     """
     Allow clients to adjust the cart content during checkout
     """
-    if request.POST.get('quantity') == '':
-        request.session['cart'] = request.session.get('cart', {})
+    if request.user.is_authenticated:
+        if request.POST.get('quantity') == '':
+            request.session['cart'] = request.session.get('cart', {})
+            return redirect(reverse('checkout'))
+        else:
+            quantity = int(request.POST.get('quantity'))
+            cart = request.session.get('cart', {})
+        
+        if quantity > 0:
+            cart[id] = quantity
+        else:
+            cart.pop(id)
+            
+        request.session['cart'] = cart
         return redirect(reverse('checkout'))
     else:
-        quantity = int(request.POST.get('quantity'))
-        cart = request.session.get('cart', {})
-    
-    if quantity > 0:
-        cart[id] = quantity
-    else:
-        cart.pop(id)
-        
-    request.session['cart'] = cart
-    return redirect(reverse('checkout'))
+        return redirect(reverse('index'))
