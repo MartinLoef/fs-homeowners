220054df8ae7d015034e582b125d9fed2446729d
Martin Loef <loefmartin@gmail.com>
2019-03-15 15:27:38 +0000

    added mandatory format to event start

--- events/forms.py
+++ events/forms.py
@@ -4,6 +4,10 @@ from .models import Event, EventComment
 class EventPostForm(forms.ModelForm):
     class Meta:
         model = Event
+        labels = {
+            'scheduled_date_start': 'scheduled date start <br /> YYYY-MM-DD HH-MM-SS',
+            'scheduled_date_end': 'scheduled date end <br /> YYYY-MM-DD HH-MM-SS',
+        }
         exclude = ["author", "views"]
 
 class EventCommentForm(forms.Form):
